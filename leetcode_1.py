3f433ee057444340430bc7ed59751e403722e945
Venkat3103 <venkatasubramanian2001@gmail.com>
2024-01-07 03:22:09 +0300

    leetcode_1 solved - optimal

--- leetcode_1.py
+++ leetcode_1.py
@@ -1,10 +1,15 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        nums_dict = {}
+
         for i in range(0,len(nums)):
-            search = target - nums[i]
-            if search in nums:
-                i2 = nums.index(search)
-                if(i!=i2):
-                    out = [i,i2]
-                    return out
+            val = nums[i]
+            to_search = target-val
+            if to_search in nums_dict:
+                ind = nums_dict[to_search]
+                return [i,ind]
+            if val not in nums_dict:
+                nums_dict[val] = i
+
 #https://leetcode.com/problems/two-sum/description/
